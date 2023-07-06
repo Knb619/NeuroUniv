5580a5383c743bf29f7df09649bb7eb571119af5
Knb619 <y.kanebako@neuro-tech.jp>
2023-07-06 16:43:57 +0900

    initial commit

---
+++ 01_Tree探索/Tree/src/Tree/TreeSearch.java
@@ -0,0 +1,27 @@
+package Tree;
+
+public class TreeSearch {
+	public void order(int searchBase) {
+		int tree[][] = new int[][] {{2, 3}, {4, 5}, {6, 7}, 
+			{8, 9}, {10, 11}, {12, 13},
+			{14}, {}, {}, {}, {}, {}, {}, {}};
+		
+		if (tree[searchBase -1].length == 2) {
+			order(tree[searchBase - 1][0]);
+			System.out.print(searchBase + " ");
+			order(tree[searchBase - 1][1]);
+			
+		} else if (tree[searchBase -1].length == 1) {
+			order(tree[searchBase - 1][0]);
+			System.out.print(searchBase + " ");
+			
+		} else {
+			System.out.print(searchBase + " ");
+		}
+	}
+	
+	public static void main(String[] args) {
+		TreeSearch tree = new TreeSearch();
+		tree.order(1);
+	}
+}
