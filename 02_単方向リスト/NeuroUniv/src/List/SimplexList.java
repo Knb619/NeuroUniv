644a057137412172f72a63d10064f4b7a0e7942d
Knb619 <y.kanebako@neuro-tech.jp>
2023-07-12 18:44:34 +0900

    単方向リスト機能追加

--- 02_単方向リスト/NeuroUniv/src/List/SimplexList.java
+++ 02_単方向リスト/NeuroUniv/src/List/SimplexList.java
@@ -1,6 +1,6 @@
 package List;
 
-// 単方向リストのノード
+// 単方向リストの要素
 class ListElement<E> {
 	E val;
 	ListElement<E> next;
@@ -17,57 +17,83 @@ public class SimplexList<E> {
 	
 	// コンストラクタ
 	public SimplexList() {
-		this.listHead = null;
+		this.listHead = null; // 先頭要素を初期化
 	}
 	
-	// リストに要素追加
+	// リストの末尾に要素追加
 	public void add(E val) {
-		ListElement<E> newElement = new ListElement<>(val);
+		ListElement<E> newElement = new ListElement<>(val); // ListElement<E> 型をインスタンス化し新たな要素を格納する
 		
 		if (listHead == null) {
-			listHead = newElement;
-		} else {
-			ListElement<E> current = listHead;
+			listHead = newElement; // Listの先頭要素を未取得の場合は取得
 			
-			while (current.next != null) {
-				current = current.next;
+		} else {
+			ListElement<E> current = listHead; // 先頭要素を 現在見ている要素として控えておく
+			
+			while (current.next != null) { // Listの最終要素に到達するまで継続
+				current = current.next; // 次の要素が存在する場合は、現在の要素を次の要素で上書き
 			}
 			
-			current.next = newElement;
+			current.next = newElement; // Listの最後尾に要素を追加(最後尾の次の参照先に、追加する要素を登録)
 		}
+	}
+	
+	// リストの指定位置に値を追加する
+	public void add(E val, int pos) {
+		
 	}
 	
 	// リストの要素削除
 	public void delNode(int pos) {
-		if (pos > 0 || listHead == null) {
+		if (pos < 0) {
+			System.out.println("[ERROR] 削除位置に負の数は指定できません");
 			return;
 		}
 		
-		if (pos == 0) {
-			listHead = listHead.next;
-		} else {
-			ListElement<E> prev = listHead;
-			ListElement<E> current = listHead.next;
+		if (pos == 0 && listHead != null) { // 先頭要素の削除のとき
+			listHead = listHead.next; // 先頭要素を次の要素で上書き
+			return;
 			
-			int i = 0;
+		} else { // 2番目の要素以降の削除のとき
+			ListElement<E> prev = listHead; // 先頭要素を控えておく
+			ListElement<E> current = listHead.next; // 現在の要素として、2番目の要素を控えておく
 			
-			while (current != null && i <pos) {
-				prev = current;
-				current = current.next;
+			int i = 1;
+			
+			while (current != null && i < pos) { // 削除位置を特定
+				// 確認位置を1つ進める
+				prev = current; // 1つ前の要素を更新
+				current = current.next; // 現在の要素を更新
 				i++;
 			}
 			
 			if (current != null) {
-				prev.next = current.next;
+				prev.next = current.next; // 1つ前の要素の次の参照先を、現在の要素の次の要素で上書きする(現在の要素をスキップする)
 			}
 		}
 	}
 	
 	// リストの長さを取得する
-
-	// リストの末尾に値を追加する
-	
-	// リストの指定位置に値を追加する
+	public int getLen() {
+		ListElement<E> current = listHead;
+		
+		if (current == null) { // リストの長さが 0 の時
+			return 0;
+			
+		} else if (current.next == null) { // リストの長さが 1 のとき
+			return 1;
+		
+		} else {
+			int len = 1;
+			
+			while (current.next != null) {
+				current = current.next;
+				len++;
+			}
+			
+			return len;
+		}
+	}
 	
 	// リストをクリアする
 	
@@ -101,9 +127,10 @@ public class SimplexList<E> {
 		
 		System.out.print("◆ リストの初期状態 ◆   : ");
 		simplexList.printList();
+		System.out.print("◆ リストの長さ ◆       : " + simplexList.getLen());
 		
-		int pos = 2; // 削除位置
-		simplexList.delNode(pos);
+		int pos = 5; // 要素の削除位置
+		simplexList.delNode(pos); // 要素の削除
 		
 		System.out.print("◆ 要素を削除したあと ◆ : ");
 		simplexList.printList();
